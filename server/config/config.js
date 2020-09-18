72866d622131dc9ccc9dcb4e6047718ab8f8e041
Sandra <s.espcastillo@gmail.com>
2020-09-18 19:44:01 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/promdb';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:rBABcgBJfnqZSJQ5@cluster0.du0hd.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
