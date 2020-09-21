dbb656029faa5cd68ff6e7c0bb21306df0386948
Sandra <s.espcastillo@gmail.com>
2020-09-21 19:08:49 -0400

    correc err

--- server/server.js
+++ server/server.js
@@ -17,10 +17,7 @@ app.use(bodyParser.json())
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
         if (err) {
-            return res.status(503).json({
-                ok: false,
-                err: 'Error en conexion a la base de datos'
-            });
+            throw err;
         }
         console.log('Base de datos ONLINE');
     });
